0f58b7315c74485b4974457389fd7e40c59d2356
Anthony <svansay@pansay.com>
2021-08-13 16:32:37 +0200

    add more unit tests for organisations and matches

---
+++ src/GivingAssistant.UnitTests/MatchTestFixture.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using AutoMapper;
+using GivingAssistant.Business.Answers.Commands.Create;
+using GivingAssistant.Business.Infrastructure;
+using GivingAssistant.Business.Matches.Commands.Create;
+using GivingAssistant.Business.Matches.Queries.GetMatchesWithOrganisationsList;
+using GivingAssistant.Business.Organisations.Models;
+using GivingAssistant.Business.Questions.Mappers;
+using GivingAssistant.Persistence;
+using NUnit.Framework;
+
+namespace GivingAssistant.UnitTests
+{
+    [TestFixture]
+    public class MatchTestFixture
+    {
+        private IAmazonDynamoDB _dynamoDb;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dynamoDb = new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                ServiceURL = "http://localhost:8000"
+            });
+            _mapper = new MapperConfiguration(x => x.AddMaps(typeof(QuestionMapper).Assembly)).CreateMapper();
+
+        }
+        [TearDown]
+        public async Task ClearDatabase()
+        {
+            var toDeleteItems = await _dynamoDb.ScanAsync(new ScanRequest(Constants.TableName));
+            foreach (var deleteItem in toDeleteItems.Items)
+            {
+                await _dynamoDb.DeleteItemAsync(Constants.TableName, new Dictionary<string, AttributeValue>
+                {
+                    {"SK", new AttributeValue(deleteItem["SK"].S)},
+                    {"PK", new AttributeValue(deleteItem["PK"].S)}
+                });
+            }
+        }
+        [Test]
+        [TestCase("Anthony")]
+        public async Task EnsureMatchesArePersisted(string userId)
+        {
+            var commandHandler = new CreateMatchCommandHandler(_dynamoDb, _mapper);
+            await commandHandler.Handle(new CreateMatchCommand
+            {
+                User = userId,
+                MatchingOrganisations = new[]{
+                  new OrganisationTagMatchListModel
+                  {
+                      OrganisationId = "org-2",
+                      Score = 80,
+                      Tag = "black",
+                      Organisation = new OrganisationDetailModel
+                      {
+                          Id = "org-2"
+                          
+                      }
+                  },
+                  new OrganisationTagMatchListModel
+              {
+                  OrganisationId = "org-1",
+                  Organisation = new OrganisationDetailModel
+                  {
+                      Id = "org-1"
+                  },
+                  Score = 20,
+                  Tag = "yellow"
+              }}
+            }, CancellationToken.None);
+
+            var response = await new GetMatchesWithOrganisationsListQueryHandler(_dynamoDb, _mapper).Handle(new GetMatchesWithOrganisationsListQuery() { UserId = userId }, CancellationToken.None);
+
+            Assert.IsTrue(response.Any(x => x.Organisation.Id == "org-1" && x.Score == 20));
+            Assert.IsTrue(response.Any(x => x.Organisation.Id == "org-2" && x.Score == 80));
+        }
+    }
+}
--- src/GivingAssistant.UnitTests/OrganisationTestFixture.cs
+++ src/GivingAssistant.UnitTests/OrganisationTestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2;
@@ -7,6 +8,7 @@ using Amazon.DynamoDBv2.Model;
 using AutoMapper;
 using GivingAssistant.Business.Infrastructure;
 using GivingAssistant.Business.Organisations.Commands.Create;
+using GivingAssistant.Business.Organisations.Queries.GetByTags;
 using GivingAssistant.Business.Questions.Mappers;
 using GivingAssistant.Persistence;
 using NUnit.Framework;
@@ -81,6 +83,42 @@ namespace GivingAssistant.UnitTests
             Assert.AreEqual(response.MetaTags, createOrganisationCommand.MetaTags);
             Assert.AreEqual(response.Mission, createOrganisationCommand.Mission);
             Assert.AreEqual(response.Vision, createOrganisationCommand.Vision);
+        }
+
+        [Test]
+        public async Task EnsureOrganisationsCanBeRetrievedByTags()
+        {
+            var createOrganisationCommand = new CreateOrganisationCommand
+            {
+                Name = "Amnesty International",
+                Description = "Amnesty doet onderzoek, spreekt autoriteiten aan op mensenrechtenschendingen en roept hen publiekelijk ter verantwoording. Dat doen we namens al onze leden en schenkers. Hun betrokkenheid en verontwaardiging zet Amnesty om in daden. ",
+                ImageUrl = "https://www.amnesty-international.be/sites/all/themes/amnesty2016/images/logo-land.png",
+                Mission = "Mensenrechten verdedig je met vuur!",
+                Vision = "Samen kunnen we onrecht stoppen. De vrijheid eisen van mensen die vastzitten om wie ze zijn of waarin ze geloven. De rechten van vrouwen en meisjes beter beschermen. Een einde maken aan folteringen en de doodstraf. Jouw gift maakt een structureel verschil in het leven van mensen, dichtbij én veraf.",
+                WebsiteUrl = "https://www.amnesty-international.be/",
+                TagScores = new Dictionary<string, int>
+                {
+                    {"Internationale hulp en mensenrechten", 100},
+                    {"Mensenrechten", 80},
+                    {"Internationaal", 80},
+                },
+                MetaTags = new Dictionary<string, string>
+                {
+                    {"MetaDataOne","valueone"},
+                    {"MetaDataTwo","valuetwo"},
+                    {"MetaDataThree","valuethree"}
+                }
+            };
+
+            var response = await new CreateOrganisationCommandHandler(_dynamoDb, _mapper).Handle(createOrganisationCommand, CancellationToken.None);
+            var queryResponse = await new GetOrganisationsByTagsListQueryHandler(_dynamoDb, _mapper).Handle(
+                new GetOrganisationsByTagsListQuery
+                {
+                    Tags = new[] { createOrganisationCommand.TagScores.First().Key }
+                }, CancellationToken.None);
+
+            Assert.IsTrue(queryResponse.Any());
+            Assert.IsTrue(queryResponse.First().OrganisationId == response.Id);
         }
 
     }
