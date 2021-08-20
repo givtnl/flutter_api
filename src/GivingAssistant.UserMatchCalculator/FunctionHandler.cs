22833b5b78aa3caa7586face20b207d1511a596b
Anthony <svansay@pansay.com>
2021-08-20 11:58:46 +0200

    add a try catch to see where we are failing

--- src/GivingAssistant.UserMatchCalculator/FunctionHandler.cs
+++ src/GivingAssistant.UserMatchCalculator/FunctionHandler.cs
@@ -31,91 +31,99 @@ namespace GivingAssistant.UserMatchCalculator
 
         public async Task HandleAsync(DynamoDBEvent @event, ILambdaContext lambdaContext)
         {
-            lambdaContext.Logger.LogLine($"receiving event {System.Text.Json.JsonSerializer.Serialize(@event)}");
+            lambdaContext.Logger.LogLine("receiving event");
             foreach (var record in @event.Records)
             {
-                var document = Document.FromAttributeMap(record.EventName == OperationType.REMOVE.Value ?
-                    record.Dynamodb.OldImage : record.Dynamodb.NewImage);
-                // if its not valid, then its for another stream listener to process
-                if (!IsValid(document))
-                    continue;
-
-                // parse the question identifier and the user
-                var primaryKey = document["PK"].AsString();
-                var sortKey = document["SK"].AsString();
-
-                lambdaContext.Logger.LogLine($"Handling PK {primaryKey} & SK {sortKey}");
-
-                // example  USER#AnthonyBoetong
-                var user = primaryKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(1);
-                // example ANSWER#QUESTION#WhatIsTheMeaningLife#MyTag
-                var questionIdentifier = sortKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(2);
-
-                var tagIdentifier = sortKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(3);
-
-                if (string.IsNullOrWhiteSpace(user))
+                try
                 {
-                    lambdaContext.Logger.LogLine($"User is empty (PK:{primaryKey})(SK:{sortKey})");
-                    continue;
-                }
+                    var document = Document.FromAttributeMap(record.EventName == OperationType.REMOVE.Value ?
+              record.Dynamodb.OldImage : record.Dynamodb.NewImage);
+                    // if its not valid, then its for another stream listener to process
+                    if (!IsValid(document))
+                        continue;
 
-                if (string.IsNullOrWhiteSpace(questionIdentifier))
-                {
-                    lambdaContext.Logger.LogLine($"Question is empty (PK:{primaryKey})(SK:{sortKey})");
-                    continue;
-                }
-                if (string.IsNullOrWhiteSpace(tagIdentifier))
-                {
-                    lambdaContext.Logger.LogLine($"Tag is empty (PK:{primaryKey})(SK:{sortKey})");
-                    continue;
-                }
-                // get the tags that belong to this question
-                var tagForQuestion = await
-                    new GetQuestionTagsListQueryHandler(DynamoDbContext, Mapper)
-                    .Handle(new GetQuestionTagsListQuery(questionIdentifier), CancellationToken.None);
+                    // parse the question identifier and the user
+                    var primaryKey = document["PK"].AsString();
+                    var sortKey = document["SK"].AsString();
 
-                if (!tagForQuestion.Any())
-                {
-                    lambdaContext.Logger.LogLine($"No Tags for this question (PK:{primaryKey})(SK:{sortKey})");
-                    continue;
-                }
+                    lambdaContext.Logger.LogLine($"Handling PK {primaryKey} & SK {sortKey}");
+
+                    // example  USER#AnthonyBoetong
+                    var user = primaryKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(1);
+                    // example ANSWER#QUESTION#WhatIsTheMeaningLife#MyTag
+                    var questionIdentifier = sortKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(2);
+
+                    var tagIdentifier = sortKey.Split('#', StringSplitOptions.RemoveEmptyEntries).ElementAtOrDefault(3);
+
+                    if (string.IsNullOrWhiteSpace(user))
+                    {
+                        lambdaContext.Logger.LogLine($"User is empty (PK:{primaryKey})(SK:{sortKey})");
+                        continue;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(questionIdentifier))
+                    {
+                        lambdaContext.Logger.LogLine($"Question is empty (PK:{primaryKey})(SK:{sortKey})");
+                        continue;
+                    }
+                    if (string.IsNullOrWhiteSpace(tagIdentifier))
+                    {
+                        lambdaContext.Logger.LogLine($"Tag is empty (PK:{primaryKey})(SK:{sortKey})");
+                        continue;
+                    }
+                    // get the tags that belong to this question
+                    var tagForQuestion = await
+                        new GetQuestionTagsListQueryHandler(DynamoDbContext, Mapper)
+                        .Handle(new GetQuestionTagsListQuery(questionIdentifier), CancellationToken.None);
+
+                    if (!tagForQuestion.Any())
+                    {
+                        lambdaContext.Logger.LogLine($"No Tags for this question (PK:{primaryKey})(SK:{sortKey})");
+                        continue;
+                    }
 
 
-                // calculate the user his or her score for the tags
-                var createUserTagMatchCommandHandler = new CreateUserTagMatchCommandHandler(DynamoDbContext, Mapper);
-                foreach (var questionTagListModel in tagForQuestion)
-                {
-                    await createUserTagMatchCommandHandler.Handle(new CreateUserTagMatchCommand
+                    // calculate the user his or her score for the tags
+                    var createUserTagMatchCommandHandler = new CreateUserTagMatchCommandHandler(DynamoDbContext, Mapper);
+                    foreach (var questionTagListModel in tagForQuestion)
+                    {
+                        await createUserTagMatchCommandHandler.Handle(new CreateUserTagMatchCommand
+                        {
+                            User = user,
+                            Answer = document["Score"].AsDecimal(),
+                            Question = questionTagListModel
+                        }, CancellationToken.None);
+
+                    }
+
+                    // get the organisations that belong to these tags
+                    var matchingOrganisations = await new GetOrganisationsByTagsListQueryHandler(DynamoDbContext, Mapper).Handle(
+                        new GetOrganisationsByTagsListQuery
+                        {
+                            Tags = tagForQuestion.Select(x => x.Tag)
+                        }, CancellationToken.None);
+
+
+                    ////TODO delete organisations that no longer match for this tag or update the scores
+
+                    if (!matchingOrganisations.Any())
+                    {
+                        lambdaContext.Logger.LogLine($"No matching organisations for these Tags (PK:{primaryKey})(SK:{sortKey})");
+                        continue;
+                    }
+                    // TODO WORK IN PROGRESS
+                    // save the user matches
+                    await new CreateUserOrganisationMatchCommandHandler(DynamoDbContext, Mapper).Handle(new CreateUserOrganisationMatchCommand
                     {
                         User = user,
-                        Answer = document["Score"].AsDecimal(),
-                        Question = questionTagListModel
-                    }, CancellationToken.None)
-                        ;
-                }
-
-                // get the organisations that belong to these tags
-                var matchingOrganisations = await new GetOrganisationsByTagsListQueryHandler(DynamoDbContext, Mapper).Handle(
-                    new GetOrganisationsByTagsListQuery
-                    {
-                        Tags = tagForQuestion.Select(x => x.Tag)
+                        MatchingOrganisations = matchingOrganisations
                     }, CancellationToken.None);
-
-
-                ////TODO delete organisations that no longer match for this tag or update the scores
-
-                if (!matchingOrganisations.Any())
-                {
-                    lambdaContext.Logger.LogLine($"No matching organisations for these Tags (PK:{primaryKey})(SK:{sortKey})");
-                    continue;
                 }
-                // TODO WORK IN PROGRESS
-                // save the user matches
-                await new CreateUserOrganisationMatchCommandHandler(DynamoDbContext, Mapper).Handle(new CreateUserOrganisationMatchCommand
+                catch (Exception e)
                 {
-                    User = user,
-                    MatchingOrganisations = matchingOrganisations
-                }, CancellationToken.None);
+                    lambdaContext.Logger.LogLine(e);
+                }
+
             }
         }
 
