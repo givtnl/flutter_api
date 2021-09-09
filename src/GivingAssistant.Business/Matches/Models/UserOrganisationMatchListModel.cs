190d9bdcf6b69f3776452681017c0d8860e424d7
anthony <anthony@boutonanthony.be>
2021-09-09 09:34:09 +0200

    remove unneeded field

--- src/GivingAssistant.Business/Matches/Models/UserOrganisationMatchListModel.cs
+++ src/GivingAssistant.Business/Matches/Models/UserOrganisationMatchListModel.cs
@@ -4,7 +4,6 @@ namespace GivingAssistant.Business.Matches.Models
 {
     public class UserOrganisationMatchListModel
     {
-        public string Tag { get; set; }
         public OrganisationDetailModel Organisation { get; set; }
         public int Score { get; set; }
     }
