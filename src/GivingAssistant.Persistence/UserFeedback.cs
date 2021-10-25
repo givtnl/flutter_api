4efb01fcd61c74d337405f8c3440646a590e8609
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-10-25 20:00:21 +0200

    Forgot the email

--- src/GivingAssistant.Persistence/UserFeedback.cs
+++ src/GivingAssistant.Persistence/UserFeedback.cs
@@ -5,6 +5,7 @@ namespace GivingAssistant.Persistence
     public class UserFeedback: BaseItem
     {
         public string UserId { get; set; }
+        public string Email { get; set; }
         public FeedbackScore Feedback { get; set; }
     }
 }
