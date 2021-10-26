6c62cdb43b0727e58ca5157906e4f156ab2c5ec9
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-10-26 16:01:55 +0200

    use save async instead of batch write

--- src/GivingAssistant.Business/Feedback/Commands/Create/CreateUserFeedbackCommandHandler.cs
+++ src/GivingAssistant.Business/Feedback/Commands/Create/CreateUserFeedbackCommandHandler.cs
@@ -2,12 +2,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2.DataModel;
 using AutoMapper;
+using GivingAssistant.Business.Infrastructure;
 using MediatR;
-using Constants = GivingAssistant.Business.Infrastructure.Constants;
 
 namespace GivingAssistant.Business.Feedback
 {
-    public class CreateUserFeedbackCommandHandler: IRequestHandler<CreateUserFeedbackCommand>
+    public class CreateUserFeedbackCommandHandler : IRequestHandler<CreateUserFeedbackCommand>
     {
         private readonly IDynamoDBContext _dynamoDb;
         private readonly IMapper _mapper;
@@ -17,20 +17,13 @@ namespace GivingAssistant.Business.Feedback
             _dynamoDb = dynamoDb;
             _mapper = mapper;
         }
-        
+
         public async Task<Unit> Handle(CreateUserFeedbackCommand request, CancellationToken cancellationToken)
         {
-            var convertedModel = _mapper.Map(request, new Persistence.UserFeedback());
-
-            var writeRequest = _dynamoDb.CreateBatchWrite<Persistence.UserFeedback>(new DynamoDBOperationConfig()
+            await _dynamoDb.SaveAsync(_mapper.Map(request, new Persistence.UserFeedback()), new DynamoDBOperationConfig
             {
                 OverrideTableName = Constants.TableName
-            });
-            
-            writeRequest.AddPutItem(convertedModel);
-
-            await writeRequest.ExecuteAsync(cancellationToken);
-            
+            }, cancellationToken);
             return Unit.Value;
         }
     }
